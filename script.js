e23d73bd6524ab62295db0c8528965f473863990
Marianne Firmino <39701279+marianneoliveira93@users.noreply.github.com>
2024-08-20 17:20:19 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,25 +2,25 @@ let inputTexto = document.querySelector('input#inputTexto');
 let outputTexto = document.querySelector('input#outputTexto');
 let mensagem = document.getElementById("resposta");
 
-var button1 = document.querySelector('.layout__botao__crip');
+var button1 = document.querySelector('.layout__botao__crip'); ;
 button1.onclick = textoCrip;
 var button2 = document.querySelector('.layout__botao__descrip');
 button2.onclick = textoDescrip;
 
-document.getElementById('none').innerHTML = '';
+document.getElementById('none').innerHTML = '';   /*limpa o conteúdo html desse elemento*/
 inputTexto.focus();
 
 function textoCrip() {
     if (inputTexto.value.length == 0) {
-        document.getElementById('none').innerHTML = '<h2 id="none">Nenhuma mensagem encontrada</h2>';
+        document.getElementById('none').innerHTML = '<h2 id="none">Nenhuma mensagem encontrada</h2>'; /*caso não seja inserido msg aparecerá esse texto*/
         inputTexto.focus();
     } else {
         document.getElementById('none').innerHTML = '';
-        document.querySelector('img.apresentacao__imagem').style.display = 'none';
+        document.querySelector('img.apresentacao__imagem').style.display = 'none';  /*se tiver msg a imagem sairá e ficará apenas a msg criptografada*/
     }
 
-    let texto = inputTexto.value;
-    let textoCriptografado = texto.replace(/e/g, 'enter')
+    let texto = inputTexto.value; /*pega o valor de entrada e armazena a variavel em texto*/
+    let textoCriptografado = texto.replace(/e/g, 'enter') /*o texto será alterado mediante os parâmetros gerando o texto criptografado*/
                                    .replace(/i/g, 'imes')
                                    .replace(/a/g, 'ai')
                                    .replace(/o/g, 'ober')
@@ -50,5 +50,4 @@ function copy() {
 }
 
 
-let info = {linkedin: "https://www.linkedin.com/in/marianne-firmino-a36863174/"};
 document.getElementyById('linkedin').href = info.linkedin;
