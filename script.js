a4e9e13249e237e9f4c6e5667b75967fc470a2fa
Marianne Firmino <39701279+marianneoliveira93@users.noreply.github.com>
2024-08-20 15:14:52 -0300

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ button1.onclick = textoCrip;
 var button2 = document.querySelector('.layout__botao__descrip');
 button2.onclick = textoDescrip;
 
-document.getElementById('none').innerHTML = '<h2 id="none">Nenhuma mensagem encontrada</h2>';
+document.getElementById('none').innerHTML = '';
 inputTexto.focus();
 
 function textoCrip() {
